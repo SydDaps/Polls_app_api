7cc067b354e63ff8c486d4b257703193d73de78b
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2021-08-28 02:33:18 +0000

    before action fix

--- app/controllers/Api/V1/tokens_controller.rb
+++ app/controllers/Api/V1/tokens_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::TokensController < ApplicationController
+  skip_before_action :authenticate_request
 
-
-    def create
-        response = Auth::AuthenticateOrganizer.call(params)
-      
-        render json: {
-          success: true,
-          code: 200,
-          data: response
-        }
-      end
+  def create
+      response = Auth::AuthenticateOrganizer.call(params)
+    
+      render json: {
+        success: true,
+        code: 200,
+        data: response
+      }
+    end
 end
