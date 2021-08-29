63c8a5262f69e5793169b243502950648bfdf945
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2021-08-29 00:09:49 +0000

    Fixing params

--- app/services/poll_service/create.rb
+++ app/services/poll_service/create.rb
@@ -2,15 +2,17 @@ module PollService
     class Create < BaseService
         def initialize(poll_params)
           @organizer = poll_params[:organizer]
-          @poll_title = poll_params[:title]
-          @poll_start_at = poll_params[:start_at]
-          @poll_end_at = poll_params[:end_at]
+          @title = poll_params[:title]
+          @description = poll_params[:description]
+          @start_at = poll_params[:start_at]
+          @pnd_at = poll_params[:end_at]
         end
 
         def call
             
             poll = @organizer.polls.create(
                 title: @poll_title,
+                description: @description,
                 start_at: @poll_start_at,
                 end_at: @poll_end_at   
             )
