872c9ba7898cf49f3f80e7f59ed54bede4f01717
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2021-10-03 15:05:36 +0000

    ff

--- app/serializers/poll_serializer.rb
+++ app/serializers/poll_serializer.rb
@@ -9,7 +9,7 @@ class PollSerializer < BaseSerializer
             status: resource.status,
             total_voters: resource.total_voters,
             total_votes: resource.voters.where(voted: true).count,
-            total_voted_percentage: ((resource.total_votes.to_f / resource.total_voters) * 100.0).round(2),
+            total_voted_percentage: ((resource.voters.where(voted: true).count.to_f / resource.total_voters) * 100.0).round(2),
             sections: SectionSerializer.new( resource.sections ).serialize
         }
     end
