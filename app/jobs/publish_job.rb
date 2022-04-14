7a0b0518ed3a6669cedba4dcc160273f3f1096c1
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2022-04-14 23:44:07 +0000

    ...

--- app/jobs/publish_job.rb
+++ app/jobs/publish_job.rb
@@ -12,24 +12,24 @@ class PublishJob < ApplicationJob
       voter.update!(
         pass_key: BCrypt::Password.create(pass_key)
       )
+      if voter.email
+        mail_params = {
+          to: voter.email,
+          template_id: "d-16b4b7d6a5f94bfa992b41a43a5cdcc7",
+          template_data: {
 
-      mail_params = {
-        to: voter.email,
-        template_id: "d-16b4b7d6a5f94bfa992b41a43a5cdcc7",
-        template_data: {
-
-          pass_key: pass_key,
-          subject: "#{params[:poll].title} poll",
-          index_number: voter.index_number,
-          organization_name: params[:poll].organizer.name,
-          poll_title: params[:poll].title,
-          voter_link: "https://epic-polls.netlify.app/vote/#{params[:poll].id}",
-          start_at: "#{params[:poll].start_at.strftime("%B %d, %Y %I:%M%P")}",
-          end_at: "#{params[:poll].end_at.strftime("%B %d, %Y %I:%M%P")}"
+            pass_key: pass_key,
+            subject: "#{params[:poll].title} poll",
+            index_number: voter.index_number,
+            organization_name: params[:poll].organizer.name,
+            poll_title: params[:poll].title,
+            voter_link: "https://epic-polls.netlify.app/vote/#{params[:poll].id}",
+            start_at: "#{params[:poll].start_at.strftime("%B %d, %Y %I:%M%P")}",
+            end_at: "#{params[:poll].end_at.strftime("%B %d, %Y %I:%M%P")}"
+          }
         }
-      }
+        Sender::Mailer.send( mail_params )
 
-      Mailer::Sender.send( mail_params )
     end
 
     params[:poll].update(publish_status: Poll::PublishedStatus::PUBLISHED)
