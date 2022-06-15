be181adc00ea0ef64cde7547368adb9e251bcb08
Dapilah Sydney <51008616+SydDaps@users.noreply.github.com>
2022-06-15 12:26:37 +0000

    .

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -14,11 +14,12 @@ class Poll < ApplicationRecord
   has_many :sections, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_and_belongs_to_many :agents, dependent: :destroy
-  # has_many :voters, dependent: :destroy
+
 
   has_many :onboardings, dependent: :destroy
   has_many :voters, through: :onboardings, dependent: :destroy
 
+   # has_many :voters, dependent: :destroy
 
   validate :validate_publish_mediums
   validate :validate_meta
