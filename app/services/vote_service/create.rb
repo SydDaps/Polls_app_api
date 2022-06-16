a5fce4aefa2cb24958096082028b3bd495b3b671
Dapilah Sydney <51008616+SydDaps@users.noreply.github.com>
2022-06-16 00:25:15 +0000

    ll

--- app/services/vote_service/create.rb
+++ app/services/vote_service/create.rb
@@ -11,19 +11,22 @@ module VoteService
         def call
 
 
-            raise Exceptions::NotUniqueRecord.message("Poll starts at #{@poll.start_at.strftime("%B %d, %Y %I:%M%P")}") if @poll.status == "Not Started"
-            raise Exceptions::NotUniqueRecord.message("Poll ended") if @poll.status == "Ended"
+          raise Exceptions::NotUniqueRecord.message("Poll starts at #{@poll.start_at.strftime("%B %d, %Y %I:%M%P")}") if @poll.status == "Not Started"
+          raise Exceptions::NotUniqueRecord.message("Poll ended") if @poll.status == "Ended"
 
-            raise Exceptions::NotUniqueRecord.message("Voted already") if @voter.has_voted(@poll_id)
+          raise Exceptions::NotUniqueRecord.message("Voted already") if @voter.has_voted(@poll_id)
 
-            @votes.each do |data|
-                Vote.create(
-                    poll_id: @poll.id,
-                    section_id: data[:section_id],
-                    option_id: data[:option_id],
-                    voter_id: @voter.id
-                )
-            end
+          @votes.each do |data|
+              Vote.create(
+                  poll_id: @poll.id,
+                  section_id: data[:section_id],
+                  option_id: data[:option_id],
+                  voter_id: @voter.id
+              )
+          end
+
+          onboarding = @voter.onboardings.where(poll_id: poll_id)&.first
+          onboarding.update!(has_voted: true)
         end
     end
 end
